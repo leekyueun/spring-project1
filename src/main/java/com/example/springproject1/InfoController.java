24fa2ca2ecee61fac7f7055793ba769f3dd8e088
kyueun <rbdms1918@naver.com>
2021-12-15 16:15:50 +0900

    fix

--- src/main/java/com/example/springproject1/InfoController.java
+++ src/main/java/com/example/springproject1/InfoController.java
@@ -1,15 +1,31 @@
 package com.example.springproject1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class EventController {
+public class InfoController {
+
+    String a;
+    String b;
+
     @PostMapping("/info/{temp}/{humidity}")
     public String temp(@PathVariable String temp, @PathVariable String humidity) {
+        a = temp;
+        b = humidity;
+        return "info/info";
+    }
+
+    @GetMapping("/index")
+    public String indexPage(Model model) {
+        model.addAttribute("temp", a);
+        model.addAttribute("humidity", b);
+
         return "/info/info";
     }
 }
